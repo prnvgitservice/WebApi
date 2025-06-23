d9cc8f683e099983f0455d61d3fb446624ddc611
lohitha <lohitha.99630@gmail.com>
2025-06-23 09:21:21 +0530

    connected_mongoDb

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, 
-        {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    //     {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
 );
     console.log('✅ MongoDB connected');
   } catch (err) {
