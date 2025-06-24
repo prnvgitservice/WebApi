b4a70240c85a1cd6393ce4925ed96e5b9d2e6f22
lohitha <lohitha.99630@gmail.com>
2025-06-24 22:30:26 +0530

    create user modals controllers routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-// const userRoutes = require('./routes/user.routes');
+const authRoutes = require('./routes/authRoutes');
 // const authMiddleware = require('./middlewares/auth.middleware');
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 
-
-// app.use('/api/users', authMiddleware, userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
---
+++ src/controllers/authControllers/user.js
@@ -0,0 +1,44 @@
+const User = require('../../models/authModel/user');
+const generateToken = require('../../utils/generateToken');
+
+exports.registerUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'Email already registered' });
+
+    const user = await User.create({ name, email, password, role });
+    
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id)
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id)
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
