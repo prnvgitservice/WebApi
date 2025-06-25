b4393f71dc241377ef3d324c689f655bded35395
lohitha <lohitha.99630@gmail.com>
2025-06-25 21:42:24 +0530

    updates

---
+++ src/routes/authRoutes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { registerUser, loginUser } = require('../src/controllers/authControllers/user');
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+module.exports = router;
