1d0b5a19f32391f3ac913205c7d65afe92928989
lohitha <lohitha.99630@gmail.com>
2025-06-28 11:48:49 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -16,20 +16,3 @@ app.use((err, req, res, next) => {
 });
 
 export default app;
-
-// const express = require('express');
-// const authRoutes = require('./routes/authRoutes/');
-// // const authMiddleware = require('./middlewares/auth.middleware');
-
-// const app = express();
-
-// // Middlewares
-// app.use(express.json());
-
-// app.use('/api/auth',authRoutes);
-
-// app.get('/', (req, res) => {
-//   res.send('API is running...');
-// });
-
-// module.exports = app;
