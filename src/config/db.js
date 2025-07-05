6c28e534f5c9daad49b02652fb941681d36807e7
lohitha <lohitha.99630@gmail.com>
2025-07-05 15:57:40 +0530

    updates

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  console.log(process.env.MONGO_URI);
+  
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB connected');
